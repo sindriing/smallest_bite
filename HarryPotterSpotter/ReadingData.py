1afbf7984d9895a92a1e1ef71d627e39fc4c684c
Sindri Ingolfsson <sindrii16@ru.is>
2017-07-10 22:07:46 +0000

    baetti vid falli, lagadi txt files og baetti vid dracula

--- HarryPotterSpotter/ReadingData.py
+++ HarryPotterSpotter/ReadingData.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
-"""Word processing Zone"""
+
+"""reads a book from a txt file into a string and simplifies the text"""
 def read_book(book):
     with open(book) as TheBook:
         booktext = (TheBook.read()
@@ -11,18 +12,28 @@ def read_book(book):
                     .replace('es ', ' ')
                     .replace('ing ', ' ')
                     .replace('-', ' ')
-                    .replace('—', ' '))
+                    .replace('—', ' ')
+                    .replace('“', '')
+                    .replace('”', '')
+                    .replace('"', ''))
         booktext = re.sub('\d+', 'NUMBER', booktext)
         booktext = booktext.replace('Page | NUMBER Harry Potter and the Philosopher Stone   J SPLITK SPLIT Rowl', '')
         return booktext
-textFiles = ('HP1.txt', 'HP2.txt')
-booktext = read_book(textFiles[0])
 
-"""Get Data Zone"""
-Sentences = booktext.replace('!', ' SPLIT').replace('?', ' SPLIT').split('SPLIT')
-SentencesDF = pd.DataFrame(Sentences)
-SentencesDF.columns = ['Sentences']
-SentencesDF = SentencesDF[SentencesDF['Sentences'].str.split(' ').map(len) > 6].reset_index().drop('index', axis = 1)
+"""Takes the string text of a book and splits it into sentences longer than 5 words and returns them in a DataFrame"""
+def create_sentences(booktext, title):
+    Sentences = booktext.replace('!', ' SPLIT').replace('?', ' SPLIT').split('SPLIT')
+    SentencesDF = pd.DataFrame(Sentences)
+    SentencesDF.columns = ['Sentences']
+    SentencesDF = SentencesDF[SentencesDF['Sentences'].str.split(' ').map(len) > 6].reset_index().drop('index', axis=1)
+    SentencesDF['Title'] = title
+    return SentencesDF
 
-print(SentencesDF.iloc[8].values)
-print(SentencesDF)
+textFiles = ('HP1.txt', 'Dracula.txt')
+df = pd.DataFrame(columns = ['Sentences', 'Title'])
+for book in textFiles:
+    booktext = read_book(book)
+    tempdf = create_sentences(booktext, book)
+    print(tempdf)
+    df = df.append(tempdf)
+print(df)
