25bbc1e5d701596c39b49b3e5d4a171a2cc0c320
Sindri Ingolfsson <sindrii16@ru.is>
2017-07-10 15:43:00 +0000

    text edting

--- HarryPotterSpotter/ReadingData.py
+++ HarryPotterSpotter/ReadingData.py
@@ -2,14 +2,24 @@ import pandas as pd
 import re
 """Word processing Zone"""
 with open('HP1.txt') as FirstBook:
-    booktext = FirstBook.read().replace('\n', '').replace('s ', ' ').replace('es ', ' ').replace('ing ', ' ')
-
-
+    booktext = (FirstBook.read()
+                .replace('\n', '')
+                .replace(',', ' ')
+                .replace('.', ' SPLIT')
+                .replace('s ', ' ')
+                .replace('es ', ' ')
+                .replace('ing ', ' ')
+                .replace('-', ' ')
+                .replace('—', ' '))
+    booktext = re.sub('\d+', 'NUMBER', booktext)
+    booktext = booktext.replace('Page | NUMBER Harry Potter and the Philosopher Stone   J SPLITK SPLIT Rowl', '')
+print(booktext)
 
 """Get Data Zone"""
-Sentences = booktext.replace('!', '.').replace('?', '.').split('.')
+Sentences = booktext.replace('!', ' SPLIT').replace('?', ' SPLIT').split('SPLIT')
 SentencesDF = pd.DataFrame(Sentences)
 SentencesDF.columns = ['Sentences']
 SentencesDF = SentencesDF[SentencesDF['Sentences'].str.split(' ').map(len) > 6].reset_index().drop('index', axis = 1)
 
+print(SentencesDF.iloc[8].values)
 print(SentencesDF)
